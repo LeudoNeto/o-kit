2c21e1432fdea1b2806ab068c87879626413aa48
Leudo Neto <100326022+LeudoNeto@users.noreply.github.com>
2023-06-19 16:33:04 +0000

    fix no coment

--- main.cpp
+++ main.cpp
@@ -89,13 +89,14 @@ int main(int argc, char** argv) {
                 solucao_vizinha[i] = solucao_vizinha[j];
                 solucao_vizinha[j] = temp;
                 
+                /*
                 custo_solucao_vizinha = custo_solucao_atual - distMatrix[solucao[i-1]-1][solucao[i]-1] - distMatrix[solucao[i+1]-1][solucao[i]-1]
                                                             + distMatrix[solucao[i-1]-1][solucao[j]-1] + distMatrix[solucao[i+1]-1][solucao[j]-1]
                                                             
                                                             - distMatrix[solucao[j-1]-1][solucao[j]-1] - distMatrix[solucao[j+1]-1][solucao[j]-1]
                                                             + distMatrix[solucao[j-1]-1][solucao[i]-1] + distMatrix[solucao[j+1]-1][solucao[i]-1];
                 
-                /*
+
                 if (custo_solucao_vizinha < custo_melhor_solucao) {
                     copia_array(melhor_solucao, solucao_vizinha, dimensao_cidades+1);
                     custo_melhor_solucao = custo_solucao_vizinha;
