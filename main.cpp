ea7bd8e7d0ca0a272484916979085f7db44eb575
Leudo Neto <100326022+LeudoNeto@users.noreply.github.com>
2023-06-21 00:18:50 +0000

    moves em funções arquivo separado

--- main.cpp
+++ main.cpp
@@ -3,23 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
+#include "etapas.c"
 
 using namespace std;
-
-double custo_solucao(short *sol, short dimensao_cidades, double **distMatrix) {
-    double custo = 0;
-    for (short i = 0; i < dimensao_cidades; i++) {
-        custo += distMatrix[sol[i]-1][sol[i+1]-1];
-        
-    }
-    return custo;
-}
-
-void copia_array(short *array_destino, short *array_origem, short size) {
-    for (short i = 0; i < size; i++) {
-        array_destino[i] = array_origem[i];
-    }
-}
 
 void preenche_sequencia_array(short *array_destino, short *array_origem, short size, short pos_inicial) {
     for (int i = 0; i < size; i++) {
@@ -27,7 +13,7 @@ void preenche_sequencia_array(short *array_destino, short *array_origem, short s
     }
 }
 
-short InArray(short *array, short num) {
+bool InArray(short *array, short num) {
     for (short i = 0; array[i] ; i++) {
         if (num == array[i]) {
             return 1;
@@ -124,112 +110,19 @@ int main(int argc, char** argv) {
         srand(time(NULL));
         
         /* 1-SWAP */
-        for (i = 1; i < dimensao_cidades; i++) {
-            for (j = i+1; j < dimensao_cidades; j++) {
-                copia_array(solucao_vizinha, solucao, dimensao_cidades+1);
-                
-                short temp = solucao_vizinha[i];
-                solucao_vizinha[i] = solucao_vizinha[j];
-                solucao_vizinha[j] = temp;
-
-                custo_solucao_vizinha = custo_solucao(solucao_vizinha, dimensao_cidades, distMatrix);
-                if (custo_solucao_vizinha < custo_melhor_solucao) {
-                    copia_array(melhor_solucao, solucao_vizinha, dimensao_cidades+1);
-                    custo_melhor_solucao = custo_solucao_vizinha;
-                    nova_solucao = 1;
-                }
-
-            }
-        }
+        swap(solucao, melhor_solucao, dimensao_cidades, custo_melhor_solucao, distMatrix);
         
         /* 2-OPT */
-        for (i = 1; i < dimensao_cidades - 3; i++) {
-            for (j = i+3; j < dimensao_cidades; j++) {
-                copia_array(solucao_vizinha, solucao, dimensao_cidades+1);
-                for (o = 0; o < (j-i+1)/2; o++) {
-                    temp = solucao_vizinha[i+o];
-                    solucao_vizinha[i+o] = solucao_vizinha[j-o];
-                    solucao_vizinha[j-o] = temp;
-                }
-
-                custo_solucao_vizinha = custo_solucao(solucao_vizinha, dimensao_cidades, distMatrix);
-                
-                if (custo_solucao_vizinha < custo_melhor_solucao) {
-                    copia_array(melhor_solucao, solucao_vizinha, dimensao_cidades+1);
-                    custo_melhor_solucao = custo_solucao_vizinha;
-                    nova_solucao = 1;
-                }
-            }
-        }
+        two_opt(solucao, melhor_solucao, dimensao_cidades, custo_melhor_solucao, distMatrix);
         
         /* REINSERTION */
-        for (i = 1; i < dimensao_cidades - 1; i++) {
-            for (j = i+1; j < dimensao_cidades; j++) {
-                copia_array(solucao_vizinha, solucao, dimensao_cidades+1);
-                short vertice_reinserido = solucao[i];
-                
-                for (o = i; o < j; o++) {
-                    solucao_vizinha[o] = solucao_vizinha[o+1];
-                }
-                solucao_vizinha[o] = vertice_reinserido;
-
-                custo_solucao_vizinha = custo_solucao(solucao_vizinha, dimensao_cidades, distMatrix);
-                
-                if (custo_solucao_vizinha < custo_melhor_solucao) {
-                    copia_array(melhor_solucao, solucao_vizinha, dimensao_cidades+1);
-                    custo_melhor_solucao = custo_solucao_vizinha;
-                    nova_solucao = 1;
-                }
-            }
-        }
+        or_opt(solucao, melhor_solucao, dimensao_cidades, custo_melhor_solucao, distMatrix, 0);
         
         /* OR-OPT-2 */
-        for (i = 1; i < dimensao_cidades - 3; i++) {
-            for (j = i+1; j < dimensao_cidades-1; j++) {
-                copia_array(solucao_vizinha, solucao, dimensao_cidades+1);
-                short vertice_reinserido1 = solucao[i];
-                short vertice_reinserido2 = solucao[i+1];
-                
-                for (o = i; o < j; o++) {
-                    solucao_vizinha[o] = solucao_vizinha[o+2];
-                }
-                solucao_vizinha[o] = vertice_reinserido1;
-                solucao_vizinha[o+1] = vertice_reinserido2;
-                
-                custo_solucao_vizinha = custo_solucao(solucao_vizinha, dimensao_cidades, distMatrix);
-                
-                if (custo_solucao_vizinha < custo_melhor_solucao) {
-                    copia_array(melhor_solucao, solucao_vizinha, dimensao_cidades+1);
-                    custo_melhor_solucao = custo_solucao_vizinha;
-                    nova_solucao = 1;
-                }
-            }
-        }
+        or_opt(solucao, melhor_solucao, dimensao_cidades, custo_melhor_solucao, distMatrix, 1);
         
         /* OR-OPT-3 */
-        for (i = 1; i < dimensao_cidades - 5; i++) {
-            for (j = i+1; j < dimensao_cidades-2; j++) {
-                copia_array(solucao_vizinha, solucao, dimensao_cidades+1);
-                short vertice_reinserido1 = solucao[i];
-                short vertice_reinserido2 = solucao[i+1];
-                short vertice_reinserido3 = solucao[i+2];
-                
-                for (o = i; o < j; o++) {
-                    solucao_vizinha[o] = solucao_vizinha[o+3];
-                }
-                solucao_vizinha[o] = vertice_reinserido1;
-                solucao_vizinha[o+1] = vertice_reinserido2;
-                solucao_vizinha[o+2] = vertice_reinserido3;
-                
-                custo_solucao_vizinha = custo_solucao(solucao_vizinha, dimensao_cidades, distMatrix);
-                
-                if (custo_solucao_vizinha < custo_melhor_solucao) {
-                    copia_array(melhor_solucao, solucao_vizinha, dimensao_cidades+1);
-                    custo_melhor_solucao = custo_solucao_vizinha;
-                    nova_solucao = 1;
-                }
-            }
-        }
+        or_opt(solucao, melhor_solucao, dimensao_cidades, custo_melhor_solucao, distMatrix, 2);
 
         /* printf("Custo solucao vizinha: %lf, Custo solucao atual: %lf\n", custo_solucao_vizinha, custo_melhor_solucao); */
         
---
+++ moves.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+double custo_solucao(short *sol, short dimensao_cidades, double **distMatrix) {
+    double custo = 0;
+    for (short i = 0; i < dimensao_cidades; i++) {
+        custo += distMatrix[sol[i]-1][sol[i+1]-1];
+        
+    }
+    return custo;
+}
+
+void copia_array(short *array_destino, short *array_origem, short size) {
+    for (short i = 0; i < size; i++) {
+        array_destino[i] = array_origem[i];
+    }
+}
+
+bool swap(short *solucao, short *melhor_solucao, short dimensao_cidades, double custo_melhor_solucao, double **distMatrix) {
+    short i, j;
+    short solucao_vizinha[dimensao_cidades+1];
+    double custo_solucao_vizinha;
+    bool nova_solucao;
+
+    for (i = 1; i < dimensao_cidades; i++) {
+        for (j = i+1; j < dimensao_cidades; j++) {
+            copia_array(solucao_vizinha, solucao, dimensao_cidades+1);
+            
+            short temp = solucao_vizinha[i];
+            solucao_vizinha[i] = solucao_vizinha[j];
+            solucao_vizinha[j] = temp;
+
+            custo_solucao_vizinha = custo_solucao(solucao_vizinha, dimensao_cidades, distMatrix);
+            if (custo_solucao_vizinha < custo_melhor_solucao) {
+                copia_array(melhor_solucao, solucao_vizinha, dimensao_cidades+1);
+                custo_melhor_solucao = custo_solucao_vizinha;
+                nova_solucao = 1;
+            }
+
+        }
+    }
+    return nova_solucao;
+}
+
+bool two_opt(short *solucao, short *melhor_solucao, short dimensao_cidades, double custo_melhor_solucao, double **distMatrix) {
+    short i, j, temp, o;
+    short solucao_vizinha[dimensao_cidades+1];
+    double custo_solucao_vizinha;
+    bool nova_solucao;
+
+    for (i = 1; i < dimensao_cidades - 3; i++) {
+        for (j = i+3; j < dimensao_cidades; j++) {
+            copia_array(solucao_vizinha, solucao, dimensao_cidades+1);
+            for (o = 0; o < (j-i+1)/2; o++) {
+                temp = solucao_vizinha[i+o];
+                solucao_vizinha[i+o] = solucao_vizinha[j-o];
+                solucao_vizinha[j-o] = temp;
+            }
+
+            custo_solucao_vizinha = custo_solucao(solucao_vizinha, dimensao_cidades, distMatrix);
+            
+            if (custo_solucao_vizinha < custo_melhor_solucao) {
+                copia_array(melhor_solucao, solucao_vizinha, dimensao_cidades+1);
+                custo_melhor_solucao = custo_solucao_vizinha;
+                nova_solucao = 1;
+            }
+        }
+    }
+    return nova_solucao;
+}
+
+bool or_opt(short *solucao, short *melhor_solucao, short dimensao_cidades, double custo_melhor_solucao, double **distMatrix, short opt_type) {
+    short i, j, temp, o;
+    short solucao_vizinha[dimensao_cidades+1];
+    double custo_solucao_vizinha;
+    bool nova_solucao;
+
+    for (i = 1; i < dimensao_cidades - (1 + 2*opt_type); i++) {
+        for (j = i+1; j < dimensao_cidades-opt_type; j++) {
+            copia_array(solucao_vizinha, solucao, dimensao_cidades+1);
+            short temp_vertices[opt_type+1];
+
+            for (temp = 0; temp <= opt_type; temp++) {
+                temp_vertices[temp] = solucao[i+temp];
+            }
+            
+            for (o = i; o < j; o++) {
+                solucao_vizinha[o] = solucao_vizinha[o+opt_type+1];
+            }
+
+            for (temp = 0; temp <= opt_type; temp++) {
+                solucao_vizinha[o+temp] = temp_vertices[temp];
+            }
+            
+            custo_solucao_vizinha = custo_solucao(solucao_vizinha, dimensao_cidades, distMatrix);
+            
+            if (custo_solucao_vizinha < custo_melhor_solucao) {
+                copia_array(melhor_solucao, solucao_vizinha, dimensao_cidades+1);
+                custo_melhor_solucao = custo_solucao_vizinha;
+                nova_solucao = 1;
+            }
+        }
+    }
+    return nova_solucao;
+}
+
